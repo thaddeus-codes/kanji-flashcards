25f473425394a6d25b0dfb140a39d441557e426d
Thaddeus Metz <temetz90@gmail.com>
2020-06-26 13:02:13 -0500

    learning animations to flip cards

--- components/kanji/KanjiCard.js
+++ components/kanji/KanjiCard.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
-import { StyleSheet, Text } from 'react-native';
-import { BaseRouter } from '@react-navigation/native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Animated,
+  TouchableOpacity,
+} from 'react-native';
 
 export default class KanjiCard extends Component {
   constructor(props) {
@@ -21,24 +27,26 @@ export default class KanjiCard extends Component {
   async getData() {
     const { data } = await axios({
       method: 'GET',
-      url: 'https://kanjialive-api.p.rapidapi.com/api/public/search/advanced/',
+      url: 'https://kanjialive-api.p.rapidapi.com/api/public/kanji/all',
       headers: {
         'content-type': 'application/octet-stream',
         'x-rapidapi-host': 'kanjialive-api.p.rapidapi.com',
         'x-rapidapi-key': '70834d5555msh146ccbf79ca3d06p140bf1jsn95e15eef3ce1',
         useQueryString: true,
       },
-      params: {
-        grade: this.props.route.params.grade,
-      },
     });
 
-    this.setState({ kanjiData: data, loading: false });
+    this.setState({
+      kanjiData: data.filter(kanji => {
+        return kanji.references.grade === this.props.route.params.grade;
+      }),
+      loading: false,
+    });
   }
 
   render() {
     let key = 0;
-    console.log(this.state.kanjiData);
+
     return (
       <CardStack
         style={styles.content}
@@ -47,7 +55,9 @@ export default class KanjiCard extends Component {
         }}
       >
         {this.state.loading ? (
-          <Text>Loading...</Text>
+          <View>
+            <Image source={require('../../assets/loading.gif')} />
+          </View>
         ) : (
           this.state.kanjiData.map(kanji => {
             key++;
@@ -55,7 +65,9 @@ export default class KanjiCard extends Component {
               <Card
                 style={[
                   styles.card,
-                  { backgroundColor: this.props.route.params.backgroundColor },
+                  {
+                    backgroundColor: this.props.route.params.backgroundColor,
+                  },
                 ]}
                 key={key}
               >
@@ -82,5 +94,6 @@ const styles = StyleSheet.create({
     color: '#ffffff',
     fontSize: 130,
     fontWeight: 'bold',
+    backfaceVisibility: 'hidden',
   },
 });
